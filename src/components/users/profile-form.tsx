2796d0fa4533bfb49f161857f5935644cb75f45f
Daniel Jhon <115909645+Ikal-Sama@users.noreply.github.com>
2024-09-02 10:09:29 +0800

    removed profile form component

--- src/components/users/profile-form.tsx
+++ src/components/users/profile-form.tsx
@@ -1,19 +1,18 @@
-import React from "react";
-import { CardWrapper } from "./card-wrapper";
+// import React from "react";
+// import { CardWrapper } from "./card-wrapper";
 
-const ProfileForm = () => {
-  return (
-    <CardWrapper
-      headerTitle="Sign in"
-      headerLabel="Welcome Back!"
-      backButtonHref=""
-      backButtonText=""
-      backButtonLabel=""
-      socialLabel=""
-    >
-      <h1>Form</h1>
-    </CardWrapper>
-  );
-};
+// const ProfileForm = () => {
+//   return (
+//     <CardWrapper
+//       headerTitle="Sign in"
+//       headerLabel="Welcome Back!"
+//       backButtonHref=""
+//       backButtonText=""
+//       backButtonLabel=""
+//     >
+//       <h1>Form</h1>
+//     </CardWrapper>
+//   );
+// };
 
-export default ProfileForm;
+// export default ProfileForm;
